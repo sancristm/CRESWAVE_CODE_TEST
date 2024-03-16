36cdab21e7f1edc5ee6cd7e6c6e0f93edeead1cd
sancristm <sancristma@gmail.com>
2024-03-16 14:36:58 +0300

    boiler plate

---
+++ src/main/java/com/example/CRESWAVE_CODE_TEST/CreswaveCodeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRESWAVE_CODE_TEST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreswaveCodeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreswaveCodeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CRESWAVE_CODE_TEST/CreswaveCodeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRESWAVE_CODE_TEST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreswaveCodeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
